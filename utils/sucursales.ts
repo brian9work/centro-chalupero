4510884134940de7217357c3a796d435e0a3d664
Brian <brian9work@gmail.com>
2026-01-16 10:17:40 -0600

    change of deloveryCost

--- utils/sucursales.ts
+++ utils/sucursales.ts
@@ -4,12 +4,15 @@ const sucursales = [
         name: "Centro Chalupero",
         address: "C. 16 de Septiembre, Centro, 90200 Heroica Cdad. de Calpulalpan, Tlax.",
         maps: "https://www.google.com/maps/place/Centro+Chalupero/@19.5892322,-98.5681138,20z/data=!4m6!3m5!1s0x85d1cd02da874d6f:0x7a1aa13714a1ae2b!8m2!3d19.5891166!4d-98.5679884!16s%2Fg%2F11n8_k9sbw?entry=ttu&g_ep=EgoyMDI2MDEwNy4wIKXMDSoASAFQAw%3D%3D",
-        embedded :`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d469.8611933034659!2d-98.56811382148692!3d19.58923218702703!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1cd02da874d6f%3A0x7a1aa13714a1ae2b!2sCentro%20Chalupero!5e0!3m2!1ses-419!2smx!4v1768364847431!5m2!1ses-419!2smx`,
+        embedded: `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d469.8611933034659!2d-98.56811382148692!3d19.58923218702703!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1cd02da874d6f%3A0x7a1aa13714a1ae2b!2sCentro%20Chalupero!5e0!3m2!1ses-419!2smx!4v1768364847431!5m2!1ses-419!2smx`,
         number: "+527491046515"
     }
 ]
 
-export const deliveryCost = 35;
+const date = new Date();
+const hour = date.getHours();
+
+export const deliveryCost = hour >= 20 ? 40 : 35;
 export const phoneNumber = "+527491046515";
 
 export default sucursales;
